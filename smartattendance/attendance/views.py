3b714dde3b7832fdef77a9dd7edb00881e6f2aa4
Dersha <dershadersha97@gmail.com>
2026-03-21 21:16:53 +0530

    attendence record update

--- smartattendance/attendance/views.py
+++ smartattendance/attendance/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import SensorRFID, Attendance, Profile
 from django.views.decorators.http import require_GET
+from django.utils import timezone
 
 
 @csrf_exempt
@@ -30,11 +31,65 @@ def send_sensor_data(request):
         "relay": "hello"
     }
 
+    latest_sensor = SensorRFID.objects.order_by('-id').first()
+
+    if not latest_sensor or not latest_sensor.rfid_data:
+        data.update({
+            "status": "error",
+            "message": "No RFID data"
+        })
+        return JsonResponse(data)
+
+    rfid_value = latest_sensor.rfid_data
+
+    
+    user = Profile.objects.filter(rfid=rfid_value).first()
+
+    if not user:
+        data.update({
+            "status": "invalid",
+            "message": "RFID not registered",
+            "rfid": rfid_value
+        })
+        return JsonResponse(data)
+
+    
+    today = timezone.now().date()
+    attendance = Attendance.objects.filter(user=user, date=today).first()
+
+    if attendance:
+        if attendance.logout_time is None:
+            attendance.logout_time = timezone.now()
+            attendance.save()
+
+            data.update({
+                "status": "logout",
+                "user": user.name
+            })
+        else:
+            data.update({
+                "status": "already_logged_out",
+                "user": user.name
+            })
+    else:
+        Attendance.objects.create(
+            user=user,
+            date=today,
+            login_time=timezone.now()
+        )
+
+        data.update({
+            "status": "login",
+            "user": user.name
+        })
+
+    return JsonResponse(data)
+
     # check the RFID data with the available users.
     # if user exists check loggedin
     # if not loggedin register the user as loggedin 
 
-    return JsonResponse(data)
+    
 
 
 
